a51f389da40a6430ded3d18598f818a576d96a37
huangle <jay3le@qq.com>
2020-10-06 17:32:18 +0800

    哈哈

---
+++ src/components/Emptry.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="emptry"></div>
+</template>
+
+<script>
+    export default {
+        name: "Emptry"
+    }
+</script>
+
+<style scoped>
+  .emptry{
+    width: 100%;
+   height: 600px;
+  }
+</style>
